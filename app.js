a8aaa452ec30a4cbef8bcbf76b4350582e87d3a6
skripakoksana <50514474+skripakoksana@users.noreply.github.com>
2019-05-19 02:06:35 +0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,37 @@
+
+
+var timeYear = prompt("Пора року","")
+
+if(timeYear == "Зима"){
+	alert("Зараз зима")
+} else if(timeYear =="зима"){
+	alert("Зараз зима")
+} else if(timeYear =="ЗИМА"){
+	alert("Зараз зима")
+}
+if(timeYear == "Літо"){
+	alert("Зараз літо")
+} else if(timeYear =="літо"){
+	alert("Зараз літо")
+} else if(timeYear =="ЛІТО"){
+	alert("Зараз літо")
+}
+
+if(timeYear == "Осінь"){
+	alert("Зараз осінь")
+} else if(timeYear =="осінь"){
+	alert("Зараз осінь")
+} else if(timeYear =="ОСІНЬ"){
+	alert("Зараз осінь")
+}
+
+if(timeYear == "Весна"){
+	alert("Зараз весна")
+} else if(timeYear =="весна"){
+	alert("Зараз весна")
+} else if(timeYear =="ВЕСНА"){
+	alert("Зараз весна")
+}
+
+
+
