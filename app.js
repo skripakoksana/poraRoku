184f8cfb1b3dbdbcb240ba4a49d3ba1b827e6eda
skripakoksana <50514474+skripakoksana@users.noreply.github.com>
2019-05-19 14:45:40 +0300

    Add files via upload

--- app.js
+++ app.js
@@ -2,13 +2,13 @@
 
 var timeYear = prompt("Пора року","")
 
-if(timeYear == "Зима" || timeYear == "зима" || timeYear == "ЗИМА" ){
+if(timeYear == "Зима" || timeYear == "ЗИма" || timeYear == "зИма" || timeYear == "зима" ){
 	alert("Зараз зима")
-} else if(timeYear == "Літо" || timeYear == "літо"|| timeYear == "ЛІТО" ){
+} else if(timeYear == "Літо" || timeYear == "ЛІто"|| timeYear == "лІто" || timeYear == "літо" ){
 	alert("Зараз літо")
-} else if(timeYear == "Осінь" || timeYear == "осінь" || timeYear == "ОСІНЬ"){
+} else if(timeYear == "Осінь" || timeYear == "ОСінь" || timeYear == "оСінь" || timeYear == "осінь"){
 	alert("Зараз осінь")
-} else if(timeYear == "Весна" || timeYear == "весна" || timeYear == "ВЕСНА" ){
+} else if(timeYear == "Весна" || timeYear == "ВЕсна" || timeYear == "вЕсна" || timeYear == "весна" ){
 	alert("Зараз весна")
 }
 
